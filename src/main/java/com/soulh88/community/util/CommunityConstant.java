058a52f51a54aa406239553111be69353bc83c83
= <=>
2023-01-10 20:08:37 +0800

    soulh88_community_push03

---
+++ src/main/java/com/soulh88/community/util/CommunityConstant.java
@@ -0,0 +1,16 @@
+package com.soulh88.community.util;
+
+public interface CommunityConstant {
+    /**
+     * 激活
+     */
+    int ACTIVATION_SUCCESS = 0;
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAIL = 2;
+}
